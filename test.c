16fb5bfe0a6275b8e4313605e6471a68510989fb
Arjit Geo <arjitgeo@gmail.com>
2024-03-07 13:42:42 +0530

    removed some unwanted things

--- test.c
+++ test.c
@@ -5,65 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// int
-// main (void)
-// {
-//   printf ("Hello, World!\n");
-
-//   darr_t *arr = darr_new (sizeof (int));
-
-//   int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//   int b[4] = { 31, 2314, 7634455, 1242465 };
-//   int c[8]
-//       = { 143631, 3426314, 9634455, 242465, 4156, 1242465, 1242465, 1242465
-//       };
-//   int aa[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//   int ba[4] = { 31, 2314, 7634455, 1242465 };
-//   int ca[8]
-//       = { 143631, 3426314, 9634455, 242465, 4156, 1242465, 1242465, 1242465
-//       };
-
-//   darr_push (arr, a, 10);
-//   darr_push (arr, b, 4);
-//   darr_push (arr, c, 8);
-//   darr_push (arr, aa, 10);
-//   darr_push (arr, ba, 4);
-//   darr_push (arr, ca, 8);
-//   darr_push (arr, a, 10);
-//   darr_push (arr, b, 4);
-//   darr_push (arr, c, 8);
-//   darr_push (arr, aa, 10);
-//   darr_push (arr, ba, 4);
-//   darr_push (arr, ca, 8);
-
-//   // snprintf
-
-//   // int *d = darr_values (arr, malloc (darr_lengthInBytes (arr)),
-//   //                       darr_lengthInBytes (arr));
-
-//   // putchar ('\n');
-
-//   // for (size_t i = 0, count = darr_length (arr); i < count; i++)
-//   //   printf ("%d\n", d[i]);
-
-//   int e = 0;
-
-//   // darr_valueAt (arr, &e, 10);
-
-//   // printf ("%d\n", e);
-//   printf ("%d\n", *(int *)darr_valueAt (arr, &e, 10));
-
-//   putchar ('\n');
-//   putchar ('\n');
-
-//   printf ("Length: -------------- %zu\n", darr_length (arr));
-//   printf ("Capacity: ------------ %zu\n", darr_capacity (arr));
-//   printf ("size of one element: - %zu\n", darr_sizeOfOneElement (arr));
-
-//   darr_delete (arr);
-//   return (0);
-// }
-
 int
 main (void)
 {
